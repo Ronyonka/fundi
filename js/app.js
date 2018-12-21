e0f92cdc6aa61fb473b6ab371b5ce3ceccd5429c
Ronyonka <rononyonka@gmail.com>
2018-12-21 11:57:54 +0300

    added final changes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-function tailor(){
-  $('#tailors').show();
-  $('.jumbotron,.col-md-3,.well').hide();
+function tailor() {
+    $('#tailors').show();
+    $('.jumbotron,.col-md-3,.well').hide();
 }
-function electrician(){
-  $('#electricians').show();
-  $('.jumbotron,.col-md-3,.well').hide();
+
+function electrician() {
+    $('#electricians').show();
+    $('.jumbotron,.col-md-3,.well').hide();
 }
-function carpenter(){
-  $('#carpenters').show();
-  $('.jumbotron,.col-md-3,.well').hide();
+
+function carpenter() {
+    $('#carpenters').show();
+    $('.jumbotron,.col-md-3,.well').hide();
 }
-function mechanic(){
-  $('#mechanics').show();
-  $('.jumbotron,.col-md-3,.well').hide();
+
+function mechanic() {
+    $('#mechanics').show();
+    $('.jumbotron,.col-md-3,.well').hide();
+}
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
 }
